cf384603e7f0a12b639291093bbaf91b22db195c
Joab Almeida <joabalmeida.dev@gmail.com>
2025-07-16 20:11:19 -0300

    frontend prototyped working

--- notes.js
+++ notes.js
@@ -1,26 +1,49 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = 4000;
+const saltRounds = 10;
+env.config();
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(passport.initialize());
+app.use(passport.session());
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "2a3a4a5a6a",
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_NOTE_DATABASE,
+  password: process.env.PG_PASSWORD,
   port: 5432,
 });
 db.connect();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 let items = [
   { id: 1, title: "Buy milk" },
   { id: 2, title: "Finish homework" },
 ];
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
 app.get("/", async (req, res) => {
   try {
